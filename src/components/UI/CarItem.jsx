bca3657874cf317e8d7745f8eee53b99aad4d2b0
lofo <luifer2503@hotmail.com>
2024-05-27 00:37:05 -0500

    chore: Improve pagination functionality in CarListing component

--- src/components/UI/CarItem.jsx
+++ src/components/UI/CarItem.jsx
@@ -7,7 +7,7 @@ const CarItem = (props) => {
   const { imgUrl, model, carName, automatic, speed, price } = props.item;
 
   return (
-    <Col lg="4" md="4" sm="6" className="mb-5">
+    <Col lg="4" md="4" sm="6" className="mb-5" >
       <div className="car__item">
         <div className="car__img">
           <img src={imgUrl} alt="" className="w-100" />
@@ -16,7 +16,7 @@ const CarItem = (props) => {
         <div className="car__item-content mt-4">
           <h4 className="section__title text-center">{carName}</h4>
           <h6 className="rent__price text-end">
-            ${price}.00 <span>/ Day</span>
+            ${price}.00 <span></span>
           </h6>
 
           <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
