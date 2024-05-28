87814590e07bfaa5ff867cf8e4cd9ab7554e0e9c
lofo <luifer2503@hotmail.com>
2024-05-27 23:36:03 -0500

    chore: Update Firebase configuration with correct domain and project ID

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,9 +8,9 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAzf-fbxdIbQUZL6FPUzHRPFOFdcXlwsNk",
-  authDomain: "VehicleHub.firebaseapp.com",
-  projectId: "VehicleHub",
-  storageBucket: "VehicleHub.appspot.com",
+  authDomain: "vehiculehub.firebaseapp.com",
+  projectId: "vehiculehub",
+  storageBucket: "vehiculehub.appspot.com",
   messagingSenderId: "121418176230",
   appId: "1:121418176230:web:741d400482003326568d27",
   measurementId: "G-LME2S944SB"
